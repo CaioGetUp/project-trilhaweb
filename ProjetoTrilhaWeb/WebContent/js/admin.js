ecaa5bb968970ba99d02a7835549b21e3b092118
caio.matos <caio_c_matos@estudante.sc.senai.br>
2021-06-11 22:00:12 -0300

    OT9 - Ajustes

--- ProjetoTrilhaWeb/WebContent/js/admin.js
+++ ProjetoTrilhaWeb/WebContent/js/admin.js
@@ -15,4 +15,20 @@ $(document).ready(function() {
 			}
 		})
 	}
+	
+	COLDIGO.exibirAviso = function(aviso) {
+		var modal = {
+			title: "Mensagem",
+			height: 250,
+			width: 400,
+			modal: true,
+			button: {
+				"OK": function() {
+					$(this).dialog("close");
+				}
+			}
+		};
+		$("#modalAviso").html(aviso);
+		$("#modalAviso").dialog(modal);
+	}
 });
--- ProjetoTrilhaWeb/WebContent/js/product.js
+++ ProjetoTrilhaWeb/WebContent/js/product.js
@@ -8,10 +8,47 @@ $(document).ready(function() {
 			type: "GET",
 			url: "/ProjetoTrilhaWeb/rest/marca/buscar",
 			success: function(marcas) {
-				alert("Sucesso!");
+				
+				if (marcas != "") {
+					
+					$("#selMarca").html("");
+					var option = document.createElement("option");
+					option.setAttribute("value", "");
+					option.innerHTML = ("Escolha");
+					$("#selMarca").append(option);
+					
+					for (var i = 0; i < marcas.length; i++) {
+						
+						var option = document.createElement("option");
+						option.setAttribute("value", marcas[i].id);
+						option.innerHTML = (marcas[i].nome);
+						$("#selMarca").append(option);
+					
+					}
+					
+				} else {
+					
+					$("#selMarca").html("");
+					var option = document.createElement("option");
+					option.setAttribute("value", "");
+					option.innerHTML = ("Erro ao carregar marcas!");
+					$("#selMarca").append(option);
+					$("#selMarca").addClass("aviso");
+					
+				}
+				
 			},
 			error: function(info) {
-				alert("Erro");
+				
+				COLDIGO.exibirAviso("Erro ao buscar as marcas: " + info.status + " - " + info.statusText);
+				
+				$("#selMarca").html("");
+				var option = document.createElement("option");
+				option.setAttribute("value", "");
+				option.innerHTML = ("Erro ao carregar marcas!");
+				$("#selMarca").append(option);
+				$("#selMarca").addClass("aviso");
+				
 			}
 		});
 		
